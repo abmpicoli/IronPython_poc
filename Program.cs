030dbd7e5c99d7604e46df5726b38e7c95ce643f
abmpicoli <abmpicoli@uol.com.br>
2022-12-05 08:48:07 -0300

    full proof of concept refactor. So far, it works!

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿using Microsoft.Scripting.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+
+/*
+ * Use case 1:
+ * call a python function with a list containing 3 complex objects.
+ * Instantiate a new list with all items where x starts with "a",
+ * preferably using an Enumerable.
+ * 
+ * Use case 2:
+ * 
+ * call a python function with a single object.
+ * It must return true if a field has some value, false otherwise.
+ * 
+ */
 
 namespace ConsoleApp1
 {
@@ -13,17 +27,56 @@ namespace ConsoleApp1
             
             python.Runtime.LoadAssembly(typeof(Program).Assembly);
             python.Runtime.LoadAssembly(typeof(Enumerable).Assembly);
+            /* Use case 1: invoke a function list */
             Microsoft.Scripting.Hosting.ScriptScope scope = python.CreateScope();
-            SomeObject input = new SomeObject();
-            input.x = "42 to you!";
-            ScriptSource source = python.CreateScriptSourceFromFile("test.py");
-            scope.SetVariable("input", input);
-            source.Execute(scope);
-            Console.WriteLine("input x after changed. Is it preciouss???");
-            Console.WriteLine(input.x);
-            Console.WriteLine("output variable. Will it only show 2 entries");
-            IEnumerable<SomeObject> output = scope.GetVariable("output");
-            Console.WriteLine(string.Join(",", output.ToList()));
+            List<SomeObject> theList = new List<SomeObject>();
+            IEnumerable<SomeObject> s = theList;
+            
+            theList.Add(new SomeObject("a"));
+            theList.Add(new SomeObject("b"));
+            theList.Add(new SomeObject("abacate"));
+            Stopwatch watch = new Stopwatch();
+            Stopwatch fullWatch = new Stopwatch();
+            fullWatch.Start();
+
+            long readTime = -1;
+            long executionTime = -1;
+            long doApplyExecutionTime = -1;
+            long doFilterExecutionTime = -1;
+            try
+            {
+                watch.Start();
+                ScriptSource source = python.CreateScriptSourceFromFile("..\\..\\..\\test.py");
+                readTime = watch.ElapsedTicks;
+                watch.Restart();
+                source.Execute(scope);
+                dynamic doApply = scope.GetVariable("doApply");
+                dynamic doFilter = scope.GetVariable("doFilter");
+                executionTime = watch.ElapsedTicks;
+                watch.Restart();
+                List<object> onlyA = ((IEnumerable<object>)doApply(theList)).ToList();
+                doApplyExecutionTime = watch.ElapsedTicks;
+                Console.WriteLine("THE LIST WITH 'A' entries = " + string.Join(',', onlyA));
+                SomeObject abacaxi = new SomeObject("abacaxi");
+                watch.Restart();
+                bool doFilterResult = doFilter(abacaxi);
+                doFilterExecutionTime = watch.ElapsedTicks;
+                Console.WriteLine("Does abacaxi starts with 'a'?" + doFilterResult);
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(string.Join(',', scope.GetVariableNames()));
+            } finally
+            {
+                Console.WriteLine("Elapsed times: readTime = " + readTime + " ; execution time = " + executionTime + " ; apply time = " + doApplyExecutionTime + " ; doFilter execution time=" + doFilterExecutionTime);
+                fullWatch.Stop();
+                Console.WriteLine("The whole process took " + fullWatch.ElapsedMilliseconds + " ms; "+fullWatch.ElapsedTicks + " ticks");
+            }
+            
+            
+            
             
             
 
@@ -33,11 +86,14 @@ namespace ConsoleApp1
     }
     public class SomeObject {
         public String x;
-        public List<SomeObject> l = new List<SomeObject>();
 
         public override string ToString()
         {
-            return x + ": " + string.Join(",", l);
+            return x;
+        }
+        public SomeObject(string v)
+        {
+            x = v;
         }
     }
 }
--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 print("Hello world! YAY!")
 import ConsoleApp1
-import System.Linq.Enumerable
-x = ConsoleApp1.SomeObject()
-x.x="Yupii one!"
-x.l.Add(ConsoleApp1.SomeObject())
-print("x.l.count:" + str(x.l.Count))
-x.l[0].x="Yupii one.one!"
-x.l.Add(ConsoleApp1.SomeObject())
-x.l[1].x="Yupii one.two!"
-output = System.Linq.Enumerable.Take(x.l,2)
-print("output=" + str(output))
-input.x=input.x + " it works my preciousssss!!!!" 
-
+import ConsoleApp1.SomeObject as SomeObject
+import System.Linq.Enumerable as Enumerable
+from System.Collections.Generic import IEnumerable, List
+import System.Func as Func
+import traceback
+def doFilter(item):
+	try:
+		return item.x.StartsWith("a")
+	except:
+		print(traceback.format_exc())
+		raise
+def doApply(list):
+	try:
+		return Enumerable.Where(list,doFilter)
+	except:
+		print(traceback.format_exc())
+		raise
